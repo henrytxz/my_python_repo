4fdf81d521b2e20a3971acb2020649a49b854103
henry <hzhang33@gmail.com>
2016-03-19 15:32:09 -0400

    spiralize intermediate commit: may deprecate the Matrix class

--- code_wars/spiralize/direction.py
+++ code_wars/spiralize/direction.py
@@ -1,8 +1,80 @@
 class Direction(object):
-    directions = ['r','d','l','u']
-
-    def __init__(self):
+    # directions = ['r','d','l','u']
+    #
+    # def __init__(self):
+    #     pass
+    @staticmethod
+    def step(self, position):
         pass
 
-    def keep_going(self):
+    @staticmethod
+    def change_direction():
         pass
+
+class Right(Direction):
+    @staticmethod
+    def step(position):
+        row, col = position
+        return (row, col+1)
+
+    @staticmethod
+    def change_direction():
+        return Down()
+
+    def __str__(self):
+        return "r"
+
+class Down(Direction):
+    @staticmethod
+    def step(position):
+        row, col = position
+        return (row+1, col)
+
+    @staticmethod
+    def change_direction():
+        return Left()
+
+    def __str__(self):
+        return "d"
+
+class Left(Direction):
+    @staticmethod
+    def step(position):
+        row, col = position
+        return (row, col-1)
+
+    @staticmethod
+    def change_direction():
+        return Up()
+
+    def __str__(self):
+        return "l"
+
+class Up(Direction):
+    @staticmethod
+    def step(position):
+        row, col = position
+        return (row-1, col)
+
+    @staticmethod
+    def change_direction():
+        return Right()
+
+    def __str__(self):
+        return "u"
+
+if __name__ == '__main__':
+    assert str(Right.change_direction()) == 'd'
+    assert str(Down.change_direction()) == 'l'
+    assert str(Left.change_direction()) == 'u'
+    assert str(Up.change_direction()) == 'r'
+
+    position = (1,1)
+    assert Right.step(position) == (1,2)
+    assert Down.step(position) == (2,1)
+    assert Left.step(position) == (1,0)
+    assert Up.step(position) == (0,1)
+
+
+
+    # todo turn into unittest
--- code_wars/spiralize/main.py
+++ code_wars/spiralize/main.py
@@ -1,73 +1,99 @@
 from matrix import Matrix
-from direction import Direction
+from direction import Direction, Right
 
 def spiralize(size):
     mat = Matrix(size).get_matrix()
     mat[0][0] = 1
-    direction = 'r'
-    go_algo = Go(mat, (0,0), direction)
-    while not Go.done(mat, (0,0), direction):
+    # direction = 'r'
+    go_algo = Go(mat, (0,0), Right())
+    while not Go.done():
         go_algo.keep_going_in_1_direction()
     return mat
-    
+
 class Go(object):
     def __init__(self, mat, curr_pos, direction):
+        """
+
+        :param mat:
+        :param curr_pos:  a pair tuple
+        :param direction:
+        :return:
+        """
         self.mat = mat
         self.curr_pos = curr_pos
+        # self.hypothetical_pos = curr_pos
         self.direction = direction
 
-    def __done_with_direction(self):
-        return True
+    def done_with_direction(self):
+        hypothetical_pos = self.direction.step(self.curr_pos)
+        if self.out_of_bound(hypothetical_pos):
+            self.direction = self.direction.change_direction()
+            return True
+
+        # if taking 2 steps would overlap another point of the spiral => done
+        hypothetical_pos = self.direction.step(hypothetical_pos)
+        row, col = hypothetical_pos
+        if not self.out_of_bound(hypothetical_pos) \
+                and self.mat[row][col] == 1:
+            self.direction = self.direction.change_direction()
+            return True
+
+        return False
 
     def keep_going_in_1_direction(self):
-        while not self.__done_with_direction():
+        while not self.done_with_direction():
             self.step()
 
     @staticmethod   #todo probably make instance method
-    def done(mat, curr_pos, direction):
+    def done():
         return True
 
     def step(self):
         pass
 
+    def out_of_bound(self, hypothetical_pos):
+        row, col = hypothetical_pos
+        if row >= len(self.mat) or row<0: return True
+        if col >= len(self.mat) or col<0: return True
+        return False
 
 class Validate(object):
   @staticmethod
   def assert_equals(x, y):
     try:
-      assert x==y
+        assert x==y
     except:
-      print 'assert failed'
-      print 'Produced:'
-      Matrix.print_list_of_lists(x)
-      print 'Expected:'
-      Matrix.print_list_of_lists(y)
-      print '='*88
-      # y.print_matrix()
-      # print 'x:{0}'.format(x)
-      # print 'y:{0}'.format(y)
-      
+        print 'assert failed'
+        print 'Produced:'
+        Matrix.print_list_of_lists(x)
+        print 'Expected:'
+        Matrix.print_list_of_lists(y)
+        print '='*88
+
       
 if __name__ == '__main__':
-  Validate.assert_equals(spiralize(1), [[1]])
-  Validate.assert_equals(spiralize(2), [[1,1],
-                                        [0,1]])
-  # Validate.assert_equals(spiralize(3), [[1,1,1],
-  #                                   [0,0,1],
-  #                                   [1,1,1]])
-  # Validate.assert_equals(spiralize(5), [[1,1,1,1,1],
-  #                                   [0,0,0,0,1],
-  #                                   [1,1,1,0,1],
-  #                                   [1,0,0,0,1],
-  #                                   [1,1,1,1,1]])
-  # Validate.assert_equals(spiralize(8), [[1,1,1,1,1,1,1,1],
-  #                                   [0,0,0,0,0,0,0,1],
-  #                                   [1,1,1,1,1,1,0,1],
-  #                                   [1,0,0,0,0,1,0,1],
-  #                                   [1,0,1,0,0,1,0,1],
-  #                                   [1,0,1,1,1,1,0,1],
-  #                                   [1,0,0,0,0,0,0,1],
-  #                                   [1,1,1,1,1,1,1,1]])
+    assert Go(Matrix(2), (0,1), Right()).done_with_direction()
+
+
+    Validate.assert_equals(spiralize(1), [[1]])
+    Validate.assert_equals(spiralize(2), [[1,1],
+                                        [0,1]]) # todo get this to pass
+    # Validate.assert_equals(spiralize(3), [[1,1,1],
+    #                                   [0,0,1],
+    #                                   [1,1,1]])
+    # Validate.assert_equals(spiralize(5), [[1,1,1,1,1],
+    #                                   [0,0,0,0,1],
+    #                                   [1,1,1,0,1],
+    #                                   [1,0,0,0,1],
+    #                                   [1,1,1,1,1]])
+    # Validate.assert_equals(spiralize(8), [[1,1,1,1,1,1,1,1],
+    #                                   [0,0,0,0,0,0,0,1],
+    #                                   [1,1,1,1,1,1,0,1],
+    #                                   [1,0,0,0,0,1,0,1],
+    #                                   [1,0,1,0,0,1,0,1],
+    #                                   [1,0,1,1,1,1,0,1],
+    #                                   [1,0,0,0,0,0,0,1],
+    #                                   [1,1,1,1,1,1,1,1]])
     
     
     
