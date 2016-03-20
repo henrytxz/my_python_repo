37626196638a312364f77849ca319075880f8663
henry <hzhang33@gmail.com>
2016-03-20 11:41:33 -0400

    spiralize intermediate commit: worked on a whiteboard, to further simplify - get rid of the done class, the whole algo is done when num_bad_moves > 1 + other simplifications

--- code_wars/spiralize/main.py
+++ code_wars/spiralize/main.py
@@ -1,12 +1,12 @@
-from matrix import Matrix
-from direction import Direction, Right
+# from matrix import Matrix
+from direction import Direction, Right, Up
 
 def spiralize(size):
-    mat = Matrix(size).get_matrix()
+    mat = [[0]*size for i in range(size)]
     mat[0][0] = 1
     # direction = 'r'
     go_algo = Go(mat, (0,0), Right())
-    while not Go.done():
+    while not go_algo.done():
         go_algo.keep_going_in_1_direction()
     return mat
 
@@ -23,20 +23,27 @@ class Go(object):
         self.curr_pos = curr_pos
         # self.hypothetical_pos = curr_pos
         self.direction = direction
+        self.direction_and_number_steps = {}
+        self.number_steps_in_direction = 0
+
+    def switch_direction(self):
+        self.direction = self.direction.change_direction()
+        self.direction_and_number_steps[str(self.direction)] = self.number_steps_in_direction
+        return True
 
     def done_with_direction(self):
         hypothetical_pos = self.direction.step(self.curr_pos)
         if self.out_of_bound(hypothetical_pos):
-            self.direction = self.direction.change_direction()
-            return True
+            return self.switch_direction()
 
         # if taking 2 steps would overlap another point of the spiral => done
         hypothetical_pos = self.direction.step(hypothetical_pos)
         row, col = hypothetical_pos
         if not self.out_of_bound(hypothetical_pos) \
                 and self.mat[row][col] == 1:
-            self.direction = self.direction.change_direction()
-            return True
+            return self.switch_direction()
+
+
 
         return False
 
@@ -44,12 +51,21 @@ class Go(object):
         while not self.done_with_direction():
             self.step()
 
-    @staticmethod   #todo probably make instance method
-    def done():
-        return True
+    def done(self):
+        # if the algo wasn't able to step in the last direction,
+        # that means it's done
+        try:
+            return self.direction_and_number_steps.values()[-1]==0
+        except:
+            return len(self.mat)==1
 
     def step(self):
-        pass
+        row, col = self.curr_pos
+        self.mat[row][col] = 1  # mark as visited
+        new_pos = self.direction.step(self.curr_pos)
+        print 'going from {0} to {1}'.format(self.curr_pos, new_pos)
+        self.curr_pos = new_pos
+        self.number_steps_in_direction += 1
 
     def out_of_bound(self, hypothetical_pos):
         row, col = hypothetical_pos
@@ -65,22 +81,37 @@ class Validate(object):
     except:
         print 'assert failed'
         print 'Produced:'
-        Matrix.print_list_of_lists(x)
+        print_list_of_lists(x)
         print 'Expected:'
-        Matrix.print_list_of_lists(y)
+        print_list_of_lists(y)
         print '='*88
+
+def make_matrix(size):
+    return [[0]*size for i in range(size)]
+
+def print_list_of_lists(ll):
+    print 'matrix:'
+    for row in ll:
+      print row
+
 
       
 if __name__ == '__main__':
-    assert Go(Matrix(2), (0,1), Right()).done_with_direction()
-
+    m2 = make_matrix(size=2)
+    assert Go(m2, (0,0), Right()).done_with_direction() is False
+    assert Go(m2, (0,1), Right()).done_with_direction() is True
+    m3 = [[1,1,1],
+          [0,0,1],
+          [1,1,1]]
+    assert Go(m3, (2,0), Up()).done_with_direction() is True
+    # assert Go(m2, (0,1), Up()).done_with_direction() is True
 
     Validate.assert_equals(spiralize(1), [[1]])
     Validate.assert_equals(spiralize(2), [[1,1],
-                                        [0,1]]) # todo get this to pass
+                                          [0,1]])
     # Validate.assert_equals(spiralize(3), [[1,1,1],
-    #                                   [0,0,1],
-    #                                   [1,1,1]])
+    #                                       [0,0,1],
+    #                                       [1,1,1]])
     # Validate.assert_equals(spiralize(5), [[1,1,1,1,1],
     #                                   [0,0,0,0,1],
     #                                   [1,1,1,0,1],
