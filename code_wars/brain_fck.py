959818f0ca39f9abecbfd709cffe8f14cb214c2f
henry <hzhang33@gmail.com>
2016-04-10 19:36:17 -0400

    brain_fck: need to pass Hello World

--- code_wars/brain_fck.py
+++ code_wars/brain_fck.py
@@ -3,6 +3,8 @@ ops = '><+-.,[]'
 def add_diff_to_char(char, diff):
     if char == chr(255) and diff==1:
         return chr(0)
+    if char == chr(0) and diff==-1:
+        return chr(255)
     return str(unichr(ord(char) + diff))
 
 class BL:
@@ -18,23 +20,27 @@ class BL:
             op = self.code[0]
             self.code = self.code[1:]
             if op == '>':
-                # print 'i {0}, len(self.l) {1}'.format(self.i, len(self.l))
                 # increment the data pointer (to point to the next cell to the right).
-                # if self.i==len(self.l)-1:
-                #     self.l.append('')
                 self.i+=1
             elif op == '<':
                 # decrement the data pointer (to point to the next cell to the left).
                 self.i-=1
             elif op == '+':
                 # increment (increase by one) the byte at the data pointer.
-                # if self.i == len(self.l):
-                #     self.l.append(add_diff_to_char(self.l[self.i], 1))
-                # else:
-                self.l[self.i] = add_diff_to_char(self.l[self.i], 1)
+                if self.i == len(self.l):
+                    self.l.append(add_diff_to_char(self.l[self.i], 1))
+                elif self.i == -1:
+                    self.l.insert(0, add_diff_to_char(self.l[self.i], 1))
+                else:
+                    self.l[self.i] = add_diff_to_char(self.l[self.i], 1)
             elif op == '-':
                 # decrement (decrease by one) the byte at the data pointer.
-                self.l[self.i] = add_diff_to_char(self.l[self.i], -1)
+                if self.i == len(self.l):
+                    self.l.append(add_diff_to_char(self.l[self.i], -1))
+                elif self.i == -1:
+                    self.l.insert(0, add_diff_to_char(self.l[self.i], -1))
+                else:
+                    self.l[self.i] = add_diff_to_char(self.l[self.i], -1)
             elif op == '.':
                 # output the byte at the data pointer.
                 self.res.append(self.l[self.i])
@@ -55,18 +61,12 @@ class BL:
                     bl = BL(code_to_repeat, self.input, self.l, self.i)
                     self.input, res, self.l, self.i = bl.run()
                     self.res.extend(res)
-            # elif op == ']':
-            #     # if the byte at the data pointer is nonzero, then instead of moving
-            #     # the instruction pointer forward to the next command, jump it back
-            #     # to the command after the matching [ command.
-            #     return code, input, l, i, res
             else:
                 raise Exception('unexpected instruction {0}'.format(op))
         return self.input, self.res, self.l, self.i
 
     @staticmethod
     def find_closing_bracket(code):
-        # return code.rfind(']')
         d = process_brackets(code)
         return d[0]-1   # -1 because [ should be excluded
 
@@ -96,21 +96,27 @@ if __name__ == '__main__':
     # assert add_diff_to_char('C', -1) == 'B'
     #
     # # Echo until byte(255) encountered
-    code0 = ',+[-.,+]'
-    # d0 = process_brackets(code0); assert len(d0)==1; assert d0[2]==7
-    assert brain_luck(code0, 'Codewars' + chr(255)) == 'Codewars'
+    # code0 = ',+[-.,+]'
+    # # d0 = process_brackets(code0); assert len(d0)==1; assert d0[2]==7
+    # assert brain_luck(code0, 'Codewars' + chr(255)) == 'Codewars'
+    # #
+    # # # # Echo until byte(0) encountered
+    # code1 = ',[.[-],]'
+    # # d1 = process_brackets(code1); assert len(d1)==2;
+    # # assert d1[1]==7; assert d1[3]==5
+    # assert brain_luck(code1, 'Codewars' + chr(0)) == 'Codewars'
+    # #
+    # # # Two numbers multiplier
+    # # # print brain_luck(',.', chr(8) + chr(9))
+    # # code2 = '[->+>+<<]>>[-<<+>>]'
+    # # d2 = process_brackets(code2); assert len(d2)==2;
+    # # assert d2[0]==8; assert d2[11]==18
     #
-    # # # Echo until byte(0) encountered
-    code1 = ',[.[-],]'
-    # d1 = process_brackets(code1); assert len(d1)==2;
-    # assert d1[1]==7; assert d1[3]==5
-    assert brain_luck(code1, 'Codewars' + chr(0)) == 'Codewars'
-    #
-    # # Two numbers multiplier
-    # # print brain_luck(',.', chr(8) + chr(9))
-    # code2 = '[->+>+<<]>>[-<<+>>]'
-    # d2 = process_brackets(code2); assert len(d2)==2;
-    # assert d2[0]==8; assert d2[11]==18
+    # assert brain_luck(',>,<[>[->+>+<<]>>[-<<+>>]<<<-]>>.', chr(8) + chr(9)) == chr(72)  #noqa
 
-    assert brain_luck(',>,<[>[->+>+<<]>>[-<<+>>]<<<-]>>.', chr(8) + chr(9)) == chr(72)  #noqa
+    x = chr(0)
+    for i in range(72):
+        x = add_diff_to_char(x, 1)
+    print x
 
+    print brain_luck('++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.', '')
