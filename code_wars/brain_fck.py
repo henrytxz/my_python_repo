029a653cd7df518793adf5ef047175618919f729
henry <hzhang33@gmail.com>
2016-04-10 17:48:45 -0400

    brain_fck: 2 tests pass, the 3rd doesn't, need smarter bracket matching algorithm

--- code_wars/brain_fck.py
+++ code_wars/brain_fck.py
@@ -1,60 +1,73 @@
 ops = '><+-.,[]'
 
-def brain_luck(code, input):
-    global ops
-    l = []
-    i = 0   # i for index
-    res = []
-    brain_luck_recurse(code, input, l, i, res)
-    return ''.join(res)
-
 def add_diff_to_char(char, diff):
     if char == chr(255) and diff==1:
         return chr(0)
     return str(unichr(ord(char) + diff))
 
-def brain_luck_recurse(code, input, l, i, res):
-    while code:
-        op = code[0]
-        code = code[1:]
-        if op == '>':
-            # increment the data pointer (to point to the next cell to the right).
-            i+=1
-        elif op == '<':
-            # decrement the data pointer (to point to the next cell to the left).
-            i-=1
-        elif op == '+':
-            # increment (increase by one) the byte at the data pointer.
-            l[i] = add_diff_to_char(l[i], 1)
-        elif op == '-':
-            # decrement (decrease by one) the byte at the data pointer.
-            l[i] = add_diff_to_char(l[i], -1)
-        elif op == '.':
-            # output the byte at the data pointer.
-            res.append(l[i])
-        elif op == ',':
-            # accept one byte of input, storing its value in the byte at the data pointer.
-            l.insert(i, input[0])
-            input = input[1:]
-        elif op == '[':
-            # if the byte at the data pointer is zero, then instead of moving
-            # the instruction pointer forward to the next command,
-            # jump it forward to the command after the matching ] command
-            end = code.rfind(']')
-            if end == -1:
-                raise Exception('[ failed to find a matching ]')
-            code_to_repeat = code[:end]
-            while l[i] != chr(0):
-                code, input, l, i, res = brain_luck_recurse(code_to_repeat, input, l, i, res)
-            return code, input, l, i, res
-        # elif op == ']':
-        #     # if the byte at the data pointer is nonzero, then instead of moving
-        #     # the instruction pointer forward to the next command, jump it back
-        #     # to the command after the matching [ command.
-        #     return code, input, l, i, res
-        else:
-            raise Exception('unexpected instruction {0}'.format(op))
-    return code, input, l, i, res
+class BL:
+    def __init__(self, code, input, l=[]):
+        self.l = l
+        self.i = 0
+        self.res = []
+        self.code = code
+        self.input = input
+
+    def run(self):
+        while self.code:
+            op = self.code[0]
+            self.code = self.code[1:]
+            if op == '>':
+                # increment the data pointer (to point to the next cell to the right).
+                self.i+=1
+            elif op == '<':
+                # decrement the data pointer (to point to the next cell to the left).
+                self.i-=1
+            elif op == '+':
+                # increment (increase by one) the byte at the data pointer.
+                self.l[self.i] = add_diff_to_char(self.l[self.i], 1)
+            elif op == '-':
+                # decrement (decrease by one) the byte at the data pointer.
+                self.l[self.i] = add_diff_to_char(self.l[self.i], -1)
+            elif op == '.':
+                # output the byte at the data pointer.
+                self.res.append(self.l[self.i])
+            elif op == ',':
+                # accept one byte of input, storing its value in the byte at the data pointer.
+                self.l.insert(self.i, self.input[0])
+                self.input = self.input[1:]
+            elif op == '[':
+                # if the byte at the data pointer is zero, then instead of moving
+                # the instruction pointer forward to the next command,
+                # jump it forward to the command after the matching ] command
+                end = self.find_closing_bracket()
+                if end == -1:
+                    raise Exception('[ failed to find a matching ]')
+                code_to_repeat = self.code[:end]
+                self.code = self.code[end+1:]
+                while self.l[self.i] != chr(0):
+                    bl = BL(code_to_repeat, self.input, self.l)
+                    self.input, res, self.l = bl.run()
+                    self.res.extend(res)
+            # elif op == ']':
+            #     # if the byte at the data pointer is nonzero, then instead of moving
+            #     # the instruction pointer forward to the next command, jump it back
+            #     # to the command after the matching [ command.
+            #     return code, input, l, i, res
+            else:
+                raise Exception('unexpected instruction {0}'.format(op))
+        return self.input, self.res, self.l
+
+    def find_closing_bracket(self):
+        return self.code.rfind(']')
+
+
+def brain_luck(code, input):
+    bl = BL(code, input)
+    res = bl.run()[1]
+    return ''.join(res)
+
+# def brain_luck_recurse(code, input, l, i, res):
 
 if __name__ == '__main__':
     assert add_diff_to_char('C',  1) == 'D'
@@ -62,12 +75,14 @@ if __name__ == '__main__':
 
     # brain_luck('','')
     # Echo until byte(255) encountered
+    # print brain_luck(',+[-.,+]', 'Codewars' + chr(255))
     assert brain_luck(',+[-.,+]', 'Codewars' + chr(255)) == 'Codewars'
 
     # # Echo until byte(0) encountered
-    print brain_luck(',[.[-],]', 'Codewars' + chr(0))
-    # assert brain_luck(',[.[-],]', 'Codewars' + chr(0)) == 'Codewars'
+    # print brain_luck(',[.[-],]', 'Codewars' + chr(0))
+    assert brain_luck(',[.[-],]', 'Codewars' + chr(0)) == 'Codewars'
 
     # # Two numbers multiplier
-    # assert brain_luck(',>,<[>[->+>+<<]>>[-<<+>>]<<<-]>>.', chr(8) + chr(9)) == chr(72)  #noqa
+    # print brain_luck(',.', chr(8) + chr(9))
+    assert brain_luck(',>,<[>[->+>+<<]>>[-<<+>>]<<<-]>>.', chr(8) + chr(9)) == chr(72)  #noqa
 
