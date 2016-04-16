6d449784e9e8c97eb378bd731d3d525ce945458e
henry <hzhang33@gmail.com>
2016-04-16 18:48:44 -0400

    generator alt and oneof

--- design_programs/code_from_scratch/generator.py
+++ design_programs/code_from_scratch/generator.py
@@ -14,14 +14,16 @@ def lit(t):
     """
     return lambda Ns: set([t]) if (len(t) in Ns) else null
 
-# def lit(Ns):
-#     """
-#     the recognizer lit('a')('abc') = set(['a'])
-#     in other words, match(a, abc) == a
-#     we denote a by s and abc by t
-#     the generator lit('a')(Ns) =
-#     """
-#     return lambda t: set([t]) if (len(t) in Ns) else null
+def alt(x, y):
+    return lambda Ns: x(Ns) | y(Ns)
+
+def oneof(chars):
+    def f(Ns):
+        res = set()
+        for char in chars:
+            res = res.union(lit(char)(Ns))
+        return res
+    return f
 
 null = frozenset([])
 
@@ -30,13 +32,13 @@ def test():
     assert f(set([1, 2, 3, 4, 5])) == set(['hello'])
     assert f(set([1, 2, 3, 4]))    == null
 
-    # g = alt(lit('hi'), lit('bye'))
-    # assert g(set([1, 2, 3, 4, 5, 6])) == set(['bye', 'hi'])
-    # assert g(set([1, 3, 5])) == set(['bye'])
-    #
-    # h = oneof('theseletters')
-    # assert h(set([1, 2, 3])) == set(['t', 'h', 'e', 's', 'l', 'r'])
-    # assert h(set([2, 3, 4])) == null
+    g = alt(lit('hi'), lit('bye'))
+    assert g(set([1, 2, 3, 4, 5, 6])) == set(['bye', 'hi'])
+    assert g(set([1, 3, 5])) == set(['bye'])
+
+    h = oneof('theseletters')
+    assert h(set([1, 2, 3])) == set(['t', 'h', 'e', 's', 'l', 'r'])
+    assert h(set([2, 3, 4])) == null
 
     return 'tests pass'
 
