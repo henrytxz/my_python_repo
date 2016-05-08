0605a1d9c4bbcaa8f2a54d6beff4ff720acdbb83
henry <hzhang33@gmail.com>
2016-05-08 19:08:04 -0400

    completed water_pouring using dfs, try bfs next time

--- design_programs/code_from_scratch/water_pouring.py
+++ design_programs/code_from_scratch/water_pouring.py
@@ -28,27 +28,21 @@ class WaterPouring(object):
         result[fr] = total - result[to]
         return result
 
-    # def run(self):
-
     def goal_found(self, curr):
         for level in curr:
             if level == self.goal: return True
         return False
 
     def search(self, curr):
-        # print curr
         self.visited.append(curr)
         if self.goal_found(curr):
             self.goal_reached = True
-            # print 'inserting curr {0}'.format(curr)
             self.solution.insert(0, copy(curr))
             return
-        copy_curr_state = list(curr)
         for state in self.get_neighbors(curr):
             self.search(state)
             if self.goal_reached:
-                # print 'inserting curr {0}'.format(copy_curr_state)
-                self.solution.insert(0, copy_curr_state)
+                self.solution.insert(0, curr)
                 return
 
     def get_neighbors(self, curr):
@@ -63,7 +57,7 @@ class WaterPouring(object):
         def empty1(curr): return self.empty(curr, 1)
         def transfer0(curr): return self.transfer(curr, 0,1)
         def transfer1(curr): return self.transfer(curr, 1,0)
-        for action in (fill0, empty1, transfer0, transfer1, fill1, empty0):
+        for action in (fill0, fill1, empty0, empty1, transfer0, transfer1):
             if self.goal_reached:
                 return
             neighbor = action(curr)
@@ -71,28 +65,29 @@ class WaterPouring(object):
                 yield neighbor
 
 if __name__ == '__main__':
-    # test_case = WaterPouring(capacity=(9,4), goal=9)
-    # test_case.search([0,4])
-    # print test_case.solution
+    # a simple test case
+    test_case = WaterPouring(capacity=(9,4), goal=9)
+    test_case.search([0,4])
+    print '{0} \n has LENGTH {1}!'.format(test_case.solution, len(test_case.solution))
 
+    # test case from Lesson 4
     test_case0 = WaterPouring(capacity=(9,4), goal=6)
     test_case0.search([0,0])
-    print test_case0.solution
-    # [[0, 0], [9, 0], [9, 4], [0, 4], [4, 0], [4, 4], [8, 0], [8, 4], [9, 3], [0, 3], [3, 0], [3, 4], [7, 0], [7, 4], [9, 2], [0, 2], [2, 0], [2, 4], [6, 0]]
+    print '{0} \n has LENGTH {1}!'.format(test_case0.solution, len(test_case0.solution))
 
-    # test_case0 = WaterPouring(capacity=(9,4), goal=6)
-    # g = test_case0.get_neighbors([6,4])
-    # assert next(g)==[9,4]
-    # assert next(g)==[6,4]
-    # assert next(g)==[0,4]
-    # assert next(g)==[6,0]
-    #
-    # test_case1 = WaterPouring(capacity=(9,4), goal=6)
-    # assert test_case1.fill([6,1], 1) == [6,4]
-    #
-    # test_case2 = WaterPouring(capacity=(9,4), goal=6)
-    # assert test_case2.transfer([6,4], 1, 0) == [9,1]
-    #
-    # test_case3 = WaterPouring(capacity=(9,4), goal=6)
-    # assert test_case3.transfer([6,2], 0, 1) == [4,4]
+    # test get_neighbors
+    test_case0 = WaterPouring(capacity=(9,4), goal=6)
+    g = test_case0.get_neighbors([6,4])
+    assert next(g)==[9,4]
+    assert next(g)==[6,4]
+    assert next(g)==[0,4]
+    assert next(g)==[6,0]
+
+    # test the other actions
+    test_case1 = WaterPouring(capacity=(9,4), goal=6)
+    assert test_case1.fill([6,1], 1) == [6,4]
+    test_case2 = WaterPouring(capacity=(9,4), goal=6)
+    assert test_case2.transfer([6,4], 1, 0) == [9,1]
+    test_case3 = WaterPouring(capacity=(9,4), goal=6)
+    assert test_case3.transfer([6,2], 0, 1) == [4,4]
 
