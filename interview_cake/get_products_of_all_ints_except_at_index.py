110d91e416ec0136a2a7da845be11488ab99be52
henrytxz <hzhang33@gmail.com>
2016-10-31 22:01:49 -0400

    more efficient get_products_of_all_ints_except_at_index

--- interview_cake/get_products_of_all_ints_except_at_index.py
+++ interview_cake/get_products_of_all_ints_except_at_index.py
@@ -1,27 +1,21 @@
-
 def get_products_of_all_ints_except_at_index(A):
-    if not A:
-        return []
-    product_from_beg = {}
-    product_from_beg[0] = A[0]
+    if not A or len(A)==1:
+        return [1]  # product of 0 ints, think of it as x^0 => 1
+    product_before_i = [A[0]]    # 1 to make product_before_i same length as A, to make things simpler
     for i in range(1, len(A)-1):
-        product_from_beg[i] = product_from_beg[i-1] * A[i]
+        product_before_i.append(product_before_i[i-1]*A[i])
+    product_before_i.insert(0, 1)
+    product_after_i = 1
+    for i in range(len(A)-1, -1, -1):
+        curr = A[i]
+        A[i] = product_before_i[i]*product_after_i
+        product_after_i *= curr
+    return A
 
-    product_from_end = {}
-    end = len(A) - 1
-    product_from_end[end] = A[-1]
-    for j in range(end-1, 0, -1):
-        product_from_end[j] = product_from_end[j+1] * A[j]
 
-    result = [None]*len(A)
-    for k in range(len(A)):
-        if k == 0:
-            result[k] = product_from_end[k+1]
-        elif k == end:
-            result[k] = product_from_beg[k-1]
-        else:
-            result[k] = product_from_beg[k-1]*product_from_end[k+1]
-    return result
+if __name__ == '__main__':
+    A = [1,2,6,5,9]
+    assert get_products_of_all_ints_except_at_index(A) == [540, 270, 90, 108, 60]
 
-A = [1,2,6,5,9]
-assert get_products_of_all_ints_except_at_index(A) == [540, 270, 90, 108, 60]
+    A = [1,7,3,4]
+    assert get_products_of_all_ints_except_at_index(A) == [84, 12, 28, 21]
